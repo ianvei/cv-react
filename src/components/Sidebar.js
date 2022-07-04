aa030e66d41ab3994f4084d6c14137d707b279f9
ianvei <iamianveilleux@gmail.com>
2022-07-04 15:26:43 -0400

    beginning skills and education

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
 
+
 export default class Sidebar extends Component {
     constructor() {
         super()
@@ -8,6 +9,7 @@ export default class Sidebar extends Component {
         this.state = {
             // link: {link: ''},
             links: [{link: ""}],
+            isActive: false,
         }
     }
 
@@ -43,33 +45,49 @@ export default class Sidebar extends Component {
         });
     }
 
+    openForm = () => {
+        this.setState({
+            isActive: true
+        })
+    }
+
+    closeForm = () => {
+        this.setState({
+            isActive: false
+        })
+    }
+
     render() {
-        const { link, links } = this.state;
+        const { links, isActive } = this.state;
         return(
             <div className="sidebar">
-                <div className="contact-form" >
+                <button onClick={this.openForm} className="edit-overview-btn">Edit</button>
+                <div className={isActive ? "sb-links-form-visible" : 'hidden'} >
+                    <button className="close" onClick={this.closeForm}>Close</button>
                     {links.map((singleLink, index) => {
                         return(
-                            <form onSubmit={this.onSubmitLinks} key={index}>
-                        <div className="first-division">
-                            <label htmlFor="phone">Enter phone number</label>
-                                <input
-                                    onChange={(e) => this.onChange(e, index)}
-                                    value={singleLink.link}
-                                    name='link'
-                                    type="text"
-                                    id={index}
-                                />
-                            {links.length - 1 === index && <button type='submit'>Add</button>}
-                        </div>
-                        <div className="second-division">
-                            {links.length > 1 && <button type="button" className="remove-btn" onClick={() => this.onRemoveLinks(index)}>Remove</button>}
-                        </div>
-                    </form>
+                            <form onSubmit={this.onSubmitLinks} key={index}> 
+                                <div className="first-division">
+                                    <label htmlFor="phone">Enter Link Text</label>
+                                        <input
+                                            onChange={(e) => this.onChange(e, index)}
+                                            value={singleLink.link}
+                                            name='link'
+                                            type="text"
+                                            id={index}
+                                        />
+                                    {links.length - 1 === index && <button type='submit' className="add-btn">Add</button>}
+                                </div>
+                                <div className="second-division">
+                                    {links.length > 1 && <button type="button" className="remove-btn" onClick={() => this.onRemoveLinks(index)}>Remove</button>}
+                                </div>
+                            </form>
                         )
                     })}
                 </div>
+                <h1>CONTACT</h1>
                 <Contact linksToRender={links}/>
+                <h1>SKILLS</h1>
             </div>
         )
     }
